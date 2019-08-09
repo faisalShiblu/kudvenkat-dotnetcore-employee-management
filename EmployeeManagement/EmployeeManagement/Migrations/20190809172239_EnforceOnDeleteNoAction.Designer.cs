94dd41b419260e3f0cb96d9182e416bc8d759b52
Shiblu <faisalalamshiblu@gmail.com>
2019-08-09 23:26:33 +0600

    part 89

--- EmployeeManagement/EmployeeManagement/Migrations/20190809172239_EnforceOnDeleteNoAction.Designer.cs
+++ EmployeeManagement/EmployeeManagement/Migrations/20190809172239_EnforceOnDeleteNoAction.Designer.cs
@@ -4,14 +4,16 @@ using EmployeeManagement.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace EmployeeManagement.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20190809172239_EnforceOnDeleteNoAction")]
+    partial class EnforceOnDeleteNoAction
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- EmployeeManagement/EmployeeManagement/Models/AppDbContext.cs
+++ EmployeeManagement/EmployeeManagement/Models/AppDbContext.cs
@@ -19,6 +19,11 @@ namespace EmployeeManagement.Models
         {
             base.OnModelCreating(modelBuilder);
             modelBuilder.Seed();
+
+            foreach (var foreignKey in modelBuilder.Model.GetEntityTypes().SelectMany(e => e.GetForeignKeys()))
+            {
+                foreignKey.DeleteBehavior = DeleteBehavior.Restrict;
+            }
         }
     }
 }
